782ef056f7ecbda51972c868cad80075d9556861
dandes <dave.thibeault@me.com>
2022-09-13 23:56:04 -0400

    Initial Commit

---
+++ ptool-watchOS Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ptool-watchOS Watch App
+//
+//  Created by Dave Thibeault on 2022-09-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ptool-watchOS Watch App/ptool_watchOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ptool_watchOSApp.swift
+//  ptool-watchOS Watch App
+//
+//  Created by Dave Thibeault on 2022-09-13.
+//
+
+import SwiftUI
+
+@main
+struct ptool_watchOS_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
