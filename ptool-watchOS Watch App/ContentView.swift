85d379ffb11d6b134e1d4879335ed0982063a520
dandes <dave.thibeault@me.com>
2022-09-14 21:21:11 -0400

    before firebase

--- ptool-watchOS Watch App/ContentView.swift
+++ ptool-watchOS Watch App/ContentView.swift
@@ -13,7 +13,10 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hello, world! test")
+            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
         }
         .padding()
     }
