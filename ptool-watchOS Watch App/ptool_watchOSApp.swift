f919ee918e95d4c7d1fae82a7ec25ff73bb061d5
dandes <dave.thibeault@me.com>
2022-09-14 23:37:14 -0400

    apres firebase

--- ptool-watchOS Watch App/ptool_watchOSApp.swift
+++ ptool-watchOS Watch App/ptool_watchOSApp.swift
@@ -6,9 +6,12 @@
 //
 
 import SwiftUI
-
+import FirebaseCore
 @main
 struct ptool_watchOS_Watch_AppApp: App {
+    init() {
+        FirebaseApp.configure()
+      }
     var body: some Scene {
         WindowGroup {
             ContentView()
